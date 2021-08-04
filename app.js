94af22b66f4e8c2a232ba58e832df1b75df69208
George Edem <effiom123@gmail.com>
2021-08-05 00:01:59 +0200

    refactor code

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
+//Importing the http module from node
 const http = require(`http`);
-
-function rqListnr(req,res){
-
-}
-
-const server = http.createServer((req,res)=>{
-    console.log(req)
-});
-
+//Create server constant and store the http object
+//use create server method and callback function to log the request
+const server = http.createServer((req,res)=> console.log(req));
+//added an Listener Event on port 3000. When a request is made server console.log the request
 server.listen(3000);
