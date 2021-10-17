cfd2476f62fc07c83615bf29e80ce2cc4b85f685
George Edem <effiom123@gmail.com>
2021-10-18 01:06:07 +0200

    finished app attempting gitignore

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 //Importing the http module from node
 const http = require(`http`);
-//Create server constant and store the http object
+//import our custom module from routes.js file 
+const routes = require(`./routes`)
+//Create server constant and store the http object in it
 //use create server method and callback function to log the request
-const server = http.createServer((req,res)=> {
-    console.log(req.url, req.method, req.headers);
-    //res has methods which allow us to work with the server's response
-    res.setHeader(`Content-Type`, `text/html`);
-    res.write(`<html>`);
-    res.write(`<head><title>My First Page</title></head>`);
-    res.write(`<h1>Hello I'm George and this is a Server Response!</h1>`)
-    res.write(`</html>`);
-    res.end();
-    });
+const server = http.createServer(routes.handler)
+//prints out text so that I can see in terminal if its working or not
+console.log(routes.text)
 //added an Listener Event on port 3000. When a request is made server console.log the request
-server.listen(3000);
+server.listen(3000); 
+ 
+
