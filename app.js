b259ecc9a4b0afd517e1c8ad06c5951174632ef7
George Edem <effiom123@gmail.com>
2021-10-19 01:17:52 +0200

    new work on express and basic routing

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
-//Importing the http module from node
-const http = require(`http`);
-//import our custom module from routes.js file 
-const routes = require(`./routes`)
+//import express module
+const express = require(`express`);
+
+const app = express();
+
+//parser middleware usually defined before routing. 
+//used to be body-parser but has now been depreciated,
+app.use(express.urlencoded({extended:true}));
+app.use(express.json()) // To parse the incoming requests with JSON payloads
+
+app.use(`/`,(res,req, next)=>{
+    console.log(`this runs by default`);
+    next();
+})
+
+app.use(`/add-product`,(req,res,next)=>{
+    console.log(`first`);
+    //as there is a response sent we dont need to use next()
+    //nb no need to set headers nor to use res.write!
+    res.send(`<form action="/product" method="POST"><input type="text" name="title"></input><button type="submit">click me</button></form>`)
+    //if you send a response you dont want to send next();
+})
+//by using routing middleware we can determine what happens according to what type of request is made
+app.post(`/product`,(req,res,next)=>{
+    console.log(req.body)
+    res.redirect(`/`)
+})
+
+app.use(`/`,(req,res,next)=>{
+    console.log(`second`);
+    //as there is a response sent we dont need to use next()
+    //nb no need to set headers nor to use res.write!
+    res.send(`<h1>Finished!</h1>`)
+})
 //Create server constant and store the http object in it
-//use create server method and callback function to log the request
-const server = http.createServer(routes.handler)
-//prints out text so that I can see in terminal if its working or not
-console.log(routes.text)
+//use express obj and listen method to initialise and listen for the Port. 
+//No need to define server or use the http module like: const server = http.createServer(app);
 //added an Listener Event on port 3000. When a request is made server console.log the request
-server.listen(3000); 
+app.listen(3000); 
  
 
